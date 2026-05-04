891a84e6a24a40679af3cd2007810d9a10996853
polirritmico <ebray187@gmail.com>
2026-05-04 01:20:21 -0400

    feat(SaleRepository): add base implementation

---
+++ src/main/java/cl/duoc/sales/repository/SaleRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.sales.repository;
+
+import cl.duoc.sales.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {}
