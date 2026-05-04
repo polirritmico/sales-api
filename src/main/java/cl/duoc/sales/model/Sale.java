a0db935a30a2f25e88bb9bfe4881cc4a602012a2
polirritmico <ebray187@gmail.com>
2026-05-03 22:30:01 -0400

    feat(Sale): update model fields

--- src/main/java/cl/duoc/sales/model/Sale.java
+++ src/main/java/cl/duoc/sales/model/Sale.java
@@ -1,21 +1,24 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
 package cl.duoc.sales.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @Entity
@@ -31,32 +34,16 @@ public class Sale {
     private Long customerId;
 
     @Column(nullable = false)
-    private String orderReference;
-
-    @Column(nullable = false)
     private Integer amount;
 
-    // TODO: Implement SaleStatus
-    // @Column(nullable = false)
-    // @Enumerated(EnumType.IDENTITY)
-    // private SaleStatus status;
-
-    // TODO: Implement PaymentMethod
-    // @Column(nullable = false)
-    // @Enumerated(EnumType.IDENTITY)
-    // private PaymentMethod paymentMethod;
-
-    // NOTE: Maybe an id from the PaymentMicroservice response
-    @Column
-    private String transactionId;
+    @ManyToOne
+    @JoinColumn(name = "status_id", nullable = false)
+    private SaleStatus status;
 
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @Column
-    private LocalDateTime updatedAt;
-
-    @Column
-    private LocalDateTime deletedAt;
+    @Column private LocalDateTime updatedAt;
+    @Column private LocalDateTime deletedAt;
 }
