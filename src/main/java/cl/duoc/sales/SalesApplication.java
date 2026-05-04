d00b7979e8cb2b90fc42140863b0628b201ea9e4
polirritmico <ebray187@gmail.com>
2026-05-03 22:30:54 -0400

    style: apply autoformatter

--- src/main/java/cl/duoc/sales/SalesApplication.java
+++ src/main/java/cl/duoc/sales/SalesApplication.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
 package cl.duoc.sales;
 
 import org.springframework.boot.SpringApplication;
@@ -6,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SalesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
 }
--- src/test/java/cl/duoc/sales/SalesApplicationTests.java
+++ src/test/java/cl/duoc/sales/SalesApplicationTests.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
 package cl.duoc.sales;
 
 import org.junit.jupiter.api.Test;
@@ -6,8 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SalesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
