99f06e860390393ccf7c8cef8b2ed8420c5a5ea2
polirritmico <ebray187@gmail.com>
2026-05-05 01:36:13 -0400

    feat(SaleController): add findSale GET endpoint

--- src/main/java/cl/duoc/sales/controller/SaleController.java
+++ src/main/java/cl/duoc/sales/controller/SaleController.java
@@ -6,10 +6,13 @@
  */
 package cl.duoc.sales.controller;
 
+import cl.duoc.sales.dto.response.SaleResponse;
+import cl.duoc.sales.service.SaleService;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/sales")
 @RequiredArgsConstructor
 public class SaleController {
+    private final SaleService service;
 
     @GetMapping("/check-health")
     public ResponseEntity<Map<String, String>> checkHealth() {
         Map<String, String> res = Map.of("status", "SalesMicroservice Status OK.");
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SaleResponse> findSale(@PathVariable Long id) {
+        SaleResponse res = service.findById(id);
+        if (res == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(res);
     }
 }
