b8586137791eb9fc10e8bd651c6f30806990a5c8
polirritmico <ebray187@gmail.com>
2026-05-06 23:06:29 -0400

    feat(SaleController): add saveSale controller POST route

--- src/main/java/cl/duoc/sales/controller/SaleController.java
+++ src/main/java/cl/duoc/sales/controller/SaleController.java
@@ -6,14 +6,18 @@
  */
 package cl.duoc.sales.controller;
 
+import cl.duoc.sales.dto.request.SaleCreationRequest;
 import cl.duoc.sales.dto.response.SaleResponse;
 import cl.duoc.sales.service.SaleService;
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +41,10 @@ public class SaleController {
     @GetMapping
     public ResponseEntity<List<SaleResponse>> findAllSales() {
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<SaleResponse> saveSale(@Valid @RequestBody SaleCreationRequest req) {
+        return ResponseEntity.ok(service.saveSale(req));
     }
 }
