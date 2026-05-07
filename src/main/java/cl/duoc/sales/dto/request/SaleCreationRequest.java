62aff9dcfc1202ab4446d6eed95475758c64de42
polirritmico <ebray187@gmail.com>
2026-05-06 23:02:21 -0400

    feat(SaleCreationRequest): add sale creation request dto

---
+++ src/main/java/cl/duoc/sales/dto/request/SaleCreationRequest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.sales.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleCreationRequest {
+    @NotNull(message = "La venta debe estar asociada a un cliente")
+    @Positive(message = "La id del usuario no puede ser negativa")
+    private Long customerId;
+
+    @Positive(message = "El valor de venta no puede ser negativo")
+    private Integer amount;
+
+    @NotNull(message = "Los detalles de venta son obligatorios")
+    private List<SaleDetailRequest> details;
+}
