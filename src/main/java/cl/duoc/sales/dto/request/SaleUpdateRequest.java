b1bf727d65f22b46fbe60a49596452240e2e3a8a
polirritmico <ebray187@gmail.com>
2026-05-10 22:49:26 -0400

    feat(SaleUpdateRequest): add update sale request DTO

---
+++ src/main/java/cl/duoc/sales/dto/request/SaleUpdateRequest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.sales.dto.request;
+
+import cl.duoc.sales.model.SaleStatus;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleUpdateRequest {
+    @NotNull(message = "La venta debe estar asociada a un cliente")
+    @Positive(message = "La id del usuario no puede ser negativa")
+    private Long customerId;
+
+    @NotNull(message = "El valor de venta es obligatorio")
+    @Positive(message = "El valor de venta no puede ser negativo")
+    private Integer amount;
+
+    // TODO: add check annotations
+    private SaleStatus status;
+
+    @NotNull(message = "Los detalles de venta son obligatorios")
+    @Valid
+    private List<SaleDetailRequest> details;
+}
