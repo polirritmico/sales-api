4e9b0a235272c936f108d34e5f9e8457d0cd38f1
polirritmico <ebray187@gmail.com>
2026-05-13 03:57:04 -0400

    feat(SaleStatus): add response DTO

---
+++ src/main/java/cl/duoc/sales/dto/response/SaleStatusResponse.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.sales.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleStatusResponse {
+    private Integer id;
+    private String name;
+}
