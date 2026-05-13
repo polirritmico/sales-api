e94387b2581571f7f774c685bc4a5c6a6de9b785
polirritmico <ebray187@gmail.com>
2026-05-13 03:57:42 -0400

    feat(SaleUpdateRequest): add status & deleted at fields

--- src/main/java/cl/duoc/sales/dto/request/SaleUpdateRequest.java
+++ src/main/java/cl/duoc/sales/dto/request/SaleUpdateRequest.java
@@ -6,10 +6,12 @@
  */
 package cl.duoc.sales.dto.request;
 
-import cl.duoc.sales.model.SaleStatus;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,12 +25,15 @@ public class SaleUpdateRequest {
     @Positive(message = "La id del usuario no puede ser negativa")
     private Long customerId;
 
+    @NotEmpty(message = "El estado es obligatorio")
+    private String status;
+
     @NotNull(message = "El valor de venta es obligatorio")
     @Positive(message = "El valor de venta no puede ser negativo")
     private Integer amount;
 
-    // TODO: add check annotations
-    private SaleStatus status;
+    @PastOrPresent(message = "La fecha de borrado no puede ser en el futuro")
+    private LocalDateTime deletedAt;
 
     @NotNull(message = "Los detalles de venta son obligatorios")
     @Valid
