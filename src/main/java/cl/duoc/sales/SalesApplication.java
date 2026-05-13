a87069db9660350b8fcc822339e800a2f9eaf8f1
polirritmico <ebray187@gmail.com>
2026-05-13 04:04:58 -0400

    chore(cache): add spring-boot-starter-cache

--- src/main/java/cl/duoc/sales/SalesApplication.java
+++ src/main/java/cl/duoc/sales/SalesApplication.java
@@ -8,7 +8,9 @@ package cl.duoc.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SalesApplication {
 
