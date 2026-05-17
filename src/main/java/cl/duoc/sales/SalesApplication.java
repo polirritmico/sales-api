85e09be1d7a263146c1f746845e42bf877ab4954
polirritmico <ebray187@gmail.com>
2026-05-16 23:35:40 -0400

    security(SpringApplication): disable default user auto-config for JWT

--- src/main/java/cl/duoc/sales/SalesApplication.java
+++ src/main/java/cl/duoc/sales/SalesApplication.java
@@ -8,10 +8,11 @@ package cl.duoc.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class SalesApplication {
 
     public static void main(String[] args) {
