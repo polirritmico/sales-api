fa777b9f3c2d1269c0f7c993ebfaca6c3d42bd3b
polirritmico <ebray187@gmail.com>
2026-06-27 10:02:52 -0400

    feat(SalesApplication): enable eureka discovery capabilities

--- src/main/java/cl/duoc/sales/SalesApplication.java
+++ src/main/java/cl/duoc/sales/SalesApplication.java
@@ -10,9 +10,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableCaching
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class SalesApplication {
 
     public static void main(String[] args) {
